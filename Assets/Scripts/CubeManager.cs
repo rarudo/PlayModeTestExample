def6b96ea8bbd2bdd481d4011adc84c15f1e6037
rarudo <rarudonet@gmail.com>
2018-01-29 18:05:28 +0900

    イベントを追加

---
+++ Assets/Scripts/CubeManager.cs
@@ -0,0 +1,9 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class CubeManager : MonoBehaviour {
+    // cubeの座標が y < 100以下になったら呼ばれる
+    public static UnityEvent OnCubeHell = new UnityEvent();
+}
