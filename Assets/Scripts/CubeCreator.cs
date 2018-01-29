ca7cbc54aaa303cd48f3c0ae83a65247acf88461
rarudo <rarudonet@gmail.com>
2018-01-29 18:04:50 +0900

    Cubeの生成処理を追加

---
+++ Assets/Scripts/CubeCreator.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CubeCreator : MonoBehaviour
+{
+	private GameObject pCube;
+
+	// Use this for initialization
+	void Start ()
+	{
+		pCube = Resources.Load<GameObject>("Prefabs/cube");
+		StartCoroutine(CreateCubePerSecond());
+	}
+
+	IEnumerator CreateCubePerSecond()
+	{
+		while (true)
+		{
+			yield return new WaitForSeconds(1);
+			Instantiate(pCube,transform);
+		}
+	}
+}
