7487554d4a4165a3845c1625e20b3bd965c5c32d
rarudo <rarudonet@gmail.com>
2018-01-29 18:05:51 +0900

    テストを追加

---
+++ Assets/CubeTest.cs
@@ -0,0 +1,42 @@
+﻿using UnityEngine;
+using UnityEngine.TestTools;
+using NUnit.Framework;
+using System.Collections;
+using UnityEngine.SceneManagement;
+
+public class CubeTest {
+    
+    /// <summary>
+    /// [SetUp]
+    /// 各テストが呼ばれる前に毎回呼ばれる
+    /// </summary>
+    [SetUp]
+    public void Init()
+    {
+        SceneManager.LoadScene("Main");
+    }
+
+    [UnityTest]
+    public IEnumerator OnCubeHellコールバックが呼ばれるか()
+    {
+        yield return new MonoBehaviourTest<OnCubeHellCallBackTest>();
+    }
+}
+
+class OnCubeHellCallBackTest : MonoBehaviour, IMonoBehaviourTest
+{
+    /// <summary>
+    /// Trueにすると呼び出し元のyieldに戻る(テスト完了となる)
+    /// </summary>
+    public bool IsTestFinished { get; private set; }
+
+    void Start()
+    {
+        CubeManager.OnCubeHell.AddListener(OnCubeHellCallback);
+    }
+
+    void OnCubeHellCallback()
+    {
+        IsTestFinished = true;
+    }
+}
